fab9aed19087e048b4166628a222c2ff3f5e9701
Financier <mir.mir.01.2023@gmail.com>
2026-05-31 23:25:24 +0300

    feat: add config params

---
+++ config.py
@@ -0,0 +1,12 @@
+SEED = 67
+
+N_CLIENTS = 20_000
+
+DATA_START = "2022-01-31"
+DATA_END = "2026-04-30"
+
+PRODUCTS = ["deposit", "savings", "card"]
+
+DATA_DIR = "data"
+PROFILES_PATH = "data/profiles.parquet"
+MOVEMENTS_PATH = "data/movements.parquet"
